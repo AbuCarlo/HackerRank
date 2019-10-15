907716e6d52cc320aad8bf5c8e1ebef3b3edc7f7
Anthony A. Nassar <anthonyabunassar@gmail.com>
2019-10-15 13:51:05 -0400

    Still too slow after "optimizations"

--- HackerRankGroovy/src/main/groovy/com/aanassar/hackerrank/QueensAttack.groovy
+++ HackerRankGroovy/src/main/groovy/com/aanassar/hackerrank/QueensAttack.groovy
@@ -1,8 +1,56 @@
 package com.aanassar.hackerrank
 
+import java.util.stream.Collectors
+
 class QueensAttack {
 
-    static int queensAttack(reader) {
+    static class Position {
+        final int row
+        final int column
+
+        Position(int row, int column) {
+            this.row = row
+            this.column = column
+        }
+    }
+
+    static final Comparator<Position> ROW_COMPARATOR = Comparator.comparingInt({ Position p -> p.row })
+    static final Comparator<Position> COLUMN_COMPARATOR = Comparator.comparingInt({ Position p -> p.column })
+
+    static int queensAttackOptimized(Reader reader) {
+        def (int n, int k) = reader.readLine().split(' ')*.toInteger()
+        // Java is 0-based; the problem is 1-based.
+        final def (int queensRow, int queensColumn) = reader.readLine().split(' ')*.toInteger()*.minus(1)
+
+        // Track only the row of the closest diagonal obstacle.
+        final int diagonalDifference = queensRow - queensColumn
+        final int diagonalSum = queensRow + queensColumn
+
+        final List<Position> obstacles = new BufferedReader(reader).lines().map { line ->
+            int[] coordinates = line.split(' ')*.toInteger()*.minus(1)
+            new Position(coordinates[0], coordinates[1])
+        }.collect(Collectors.toList())
+
+
+        def upwardObstacle = obstacles.stream().filter { p -> p.column == queensColumn && p.row < queensRow }.max(ROW_COMPARATOR).orElse(new Position(-1, queensColumn))
+        def downwardObstacle = obstacles.stream().filter { p -> p.column == queensColumn && p.row > queensRow }.min(ROW_COMPARATOR).orElse(new Position(n, queensColumn))
+
+        def leftwardObstacle = obstacles.stream().filter { p -> p.row == queensRow && p.column < queensColumn }.max(COLUMN_COMPARATOR).orElse(new Position(queensRow, -1))
+        def rightwardObstacle = obstacles.stream().filter { p -> p.row == queensRow && p.column > queensColumn }.min(COLUMN_COMPARATOR).orElse(new Position(queensRow, n))
+
+        def northwestObstacle =  obstacles.stream().filter { p -> p.row < queensRow && p.row - p.column == diagonalDifference }.max(ROW_COMPARATOR).orElse(new Position(Math.max(-1, diagonalDifference - 1), -1))
+        def southeastObstacle =  obstacles.stream().filter { p -> p.row > queensRow && p.row - p.column == diagonalDifference }.min(ROW_COMPARATOR).orElse(new Position(Math.min(n, diagonalDifference + n), n))
+
+        def southwestObstacle =  obstacles.stream().filter { p -> p.row > queensRow && p.row + p.column == diagonalSum }.min(ROW_COMPARATOR).orElse(new Position(Math.min(n, diagonalSum + 1), -1))
+        def northeastObstacle =  obstacles.stream().filter { p -> p.row < queensRow && p.row + p.column == diagonalSum }.max(ROW_COMPARATOR).orElse(new Position(Math.max(-1, diagonalSum - n), n))
+
+        (queensRow - upwardObstacle.row - 1) + (downwardObstacle.row - queensRow - 1) +
+                (queensColumn - leftwardObstacle.column - 1) + (rightwardObstacle.column - queensColumn - 1) +
+                (queensRow - northwestObstacle.row - 1) + (queensRow - northeastObstacle.row - 1) +
+                (southwestObstacle.row - queensRow - 1) + (southeastObstacle.row - queensRow - 1)
+    }
+
+    static int queensAttack(Reader reader) {
         def (int n, int k) = reader.readLine().split(' ')*.toInteger()
         // Java is 0-based; the problem is 1-based.
         def (int queensRow, int queensColumn) = reader.readLine().split(' ')*.toInteger()*.minus(1)
@@ -12,11 +60,15 @@ class QueensAttack {
         int leftwardObstacle = -1
         int rightwardObstacle = n
 
+        final int diagonalDifference = queensRow - queensColumn
+        final int diagonalSum = queensRow + queensColumn
+
         // Track only the row of the closest diagonal obstacle.
-        int northwestObstacle = Math.max(-1, queensRow - queensColumn - 1)
-        int southwestObstacle = Math.min(n, queensRow + queensColumn + 1)
-        int northeastObstacle = Math.max(-1, queensRow - (n - queensColumn))
-        int southeastObstacle = Math.min(n, queensRow + (n - queensColumn))
+
+        int northwestObstacle = Math.max(-1, diagonalDifference - 1)
+        int southwestObstacle = Math.min(n, diagonalSum + 1)
+        int northeastObstacle = Math.max(-1, diagonalSum - n)
+        int southeastObstacle = Math.min(n, diagonalDifference + n)
 
         reader.eachLine { line ->
             // See above.
@@ -35,14 +87,14 @@ class QueensAttack {
                 } else if (row < downwardObstacle) {
                     downwardObstacle = row
                 }
-            } else if (row - column == queensRow - queensColumn) {
+            } else if (row - column == diagonalDifference) {
                 // downward diagonal
                 if (row < queensRow) {
                     if (row > northwestObstacle)
                         northwestObstacle = row
                 } else if (row < southeastObstacle)
                     southeastObstacle = row
-            } else if (row + column == queensRow + queensColumn) {
+            } else if (row + column == diagonalSum) {
                 // upward diagonal
                 if (row < queensRow) {
                     if (row > northeastObstacle)
@@ -61,7 +113,7 @@ class QueensAttack {
 
     static void main(String[] args) {
         System.in.withReader("US-ASCII") { reader ->
-            int solution = queensAttack reader
+            int solution = queensAttackOptimized reader
             println solution
         }
     }
