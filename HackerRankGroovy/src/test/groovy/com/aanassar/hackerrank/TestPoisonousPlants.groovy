52396f35679653f5920f961478b9d0fa5c2dad9d
Anthony A. Nassar <anthonyabunassar@gmail.com>
2020-01-17 22:12:24 -0500

    Poisonous Plants: correct, but too slow.

---
+++ HackerRankGroovy/src/test/groovy/com/aanassar/hackerrank/TestPoisonousPlants.groovy
@@ -0,0 +1,54 @@
+package com.aanassar.hackerrank
+
+import org.junit.Test
+
+class TestPoisonousPlants {
+
+    int countReductions(List l) {
+        final int head = l.head()
+        def counts = l.findAll({it != head}).countBy({it})
+        return counts ? counts.values().max() : 0
+    }
+
+    int poisonousPlants(List l) {
+        if (l.isEmpty())
+            return 0
+        List current = []
+        List q = [current]
+        int previous = Integer.MIN_VALUE
+        l.each { int n ->
+            if (n >= previous) {
+                current << n
+            } else {
+                current = [n]
+                q << current
+            }
+            previous = n
+        }
+        if (q.every { it.size() == 1 })
+            return 0
+        int result = q.collect { countReductions(it) }.max()
+        List next = q*.first()
+        return result + poisonousPlants(next)
+    }
+
+    int poisonousPlants(String input) {
+        List l = input.tokenize()*.toInteger().toList()
+        poisonousPlants l
+    }
+
+    @Test
+    void testSamples() {
+        assert poisonousPlants('6 5 8 4 7 10 9') == 2
+        assert poisonousPlants('3 2 5 4') == 2
+        assert poisonousPlants('4 3 7 5 6 4 2') == 3
+    }
+
+    @Test
+    void testTony() {
+        assert poisonousPlants('1 2 2 2') == 3
+        assert poisonousPlants('2 2 2 2') == 0
+        assert poisonousPlants('1 1 1 2 2 2 3 3 3') == 3
+        assert poisonousPlants((0..9).reverse().toList()) == 0
+    }
+}
