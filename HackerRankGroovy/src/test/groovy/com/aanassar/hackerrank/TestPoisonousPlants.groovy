30782dace1a0a2358c2c407db9d77c4d0f88de5d
Anthony A. Nassar <anthonyabunassar@gmail.com>
2020-01-23 20:30:31 -0500

    WIP

--- HackerRankGroovy/src/test/groovy/com/aanassar/hackerrank/TestPoisonousPlants.groovy
+++ HackerRankGroovy/src/test/groovy/com/aanassar/hackerrank/TestPoisonousPlants.groovy
@@ -44,7 +44,7 @@ class TestPoisonousPlants {
     @Test
     void testTestCaseFiles() {
 
-        // assert poisonousPlants(testFiles.resolve('input12.txt')) == 16
+        assert poisonousPlants(testFiles.resolve('input12.txt')) == 16
 
         assert poisonousPlants(testFiles.resolve('input20.txt')) == 24
 
@@ -52,6 +52,7 @@ class TestPoisonousPlants {
 
         assert poisonousPlants(testFiles.resolve('input23.txt')) == 80802
 
+        assert poisonousPlants(testFiles.resolve('input23.txt')) == 3
     }
 
     @Test
