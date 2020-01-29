e9f1dbc0e89fe188ef1c41174211fa6b224cfa5d
Anthony A. Nassar <anthonyabunassar@gmail.com>
2020-01-28 20:09:38 -0500

    Solved Poisonous Plants using Deque<>.

---
+++ HackerRankGroovy/src/main/groovy/com/aanassar/hackerrank/PoisonousPlants.java
@@ -0,0 +1,56 @@
+package com.aanassar.hackerrank;
+
+import java.util.*;
+
+public class PoisonousPlants {
+
+    static int reduce(Deque<Deque<Integer>> problem) {
+
+        int reductions = 0;
+
+        while (problem.size() > 1) {
+
+            final Deque<Deque<Integer>> nextProblem = new LinkedList<>();
+            Deque<Integer> previous = problem.removeFirst();
+            nextProblem.addLast(previous);
+
+            while (!problem.isEmpty()) {
+                final Deque<Integer> current = problem.removeFirst();
+
+                assert (current.getFirst() > previous.getLast());
+                current.removeFirst();
+
+                if (current.isEmpty()) {
+                    continue;
+                }
+                if (current.getFirst() <= previous.getLast()) {
+                    current.stream().forEach(previous::addLast);
+                } else {
+                    nextProblem.addLast(current);
+                    previous = current;
+                }
+            }
+
+            ++reductions;
+
+            problem = nextProblem;
+        }
+        return reductions;
+    }
+
+    static int poisonousPlants(int[] a) {
+        Deque<Integer> q = new LinkedList<>();
+        Deque<Deque<Integer>> problem = new LinkedList<>();
+        problem.add(q);
+        int previous = Integer.MAX_VALUE;
+        for (int n : a) {
+            if (n > previous) {
+                q = new LinkedList<>();
+                problem.add(q);
+            }
+            q.add(n);
+            previous = n;
+        }
+        return reduce(problem);
+    }
+}
--- HackerRankGroovy/src/test/groovy/com/aanassar/hackerrank/TestPoisonousPlants.groovy
+++ HackerRankGroovy/src/test/groovy/com/aanassar/hackerrank/TestPoisonousPlants.groovy
@@ -2,29 +2,82 @@ package com.aanassar.hackerrank
 
 import org.junit.Test
 
+import java.nio.file.Path
+import java.nio.file.Paths
+
 class TestPoisonousPlants {
 
-    int reduce(List l) {
-        final int head = l.head()
-        def counts = l.findAll({it != head}).countBy({it})
-        return counts ? counts.values().max() : 0
-    }
-
-    int poisonousPlants(int[] a) {
-        List l = a.collect { int n -> [ n, 1 ] }
-        reduce l
-    }
+    static final Path testFiles = Paths.get("test-files/poisonous-plants")
 
     int poisonousPlants(String input) {
         int[] l = input.tokenize()*.toInteger().toArray()
-        poisonousPlants l
+        PoisonousPlants.poisonousPlants l
+    }
+
+    int poisonousPlants(Path input) {
+        def lines = input.readLines()
+        poisonousPlants(lines[1])
+    }
+
+    int slowPoisonousPlants(List l) {
+        int reductions = 0
+        while (true) {
+            int previous = Integer.MAX_VALUE
+            List m = []
+            for (int n : l) {
+                if (n <= previous) {
+                    m << n
+                }
+                previous = n
+            }
+            if (m.size() == l.size()) {
+                return reductions
+            }
+            l = m
+            ++reductions
+        }
+    }
+
+    int poisonousPlants(List l) {
+        PoisonousPlants.poisonousPlants(l as int[])
+    }
+
+    @Test
+    void testEdgeCases() {
+        assert poisonousPlants('5 4 3 2 1 4 3 2 1 3 2 1') == 3
+        assert poisonousPlants('1 2 3 4 5 2 3 4 5 3 4 5 4 5') == 2
+        assert poisonousPlants('3 2 1 5 4 3 2') == 4
+        // gleaned from randomized tests
+        assert poisonousPlants([0, 2, 4, 3, 1] ) == 3
+        
+        assert poisonousPlants([9, 3, 5, 8, 7, 6, 4, 2, 1, 7]) == 4
+        assert poisonousPlants([2, 3, 5, 7, 6, 4, 9, 0, 1, 8]) == 3
+        assert poisonousPlants([2, 4, 0, 3, 6, 5, 1]) == 3
+    }
+
+    final Random random = new Random()
+
+    List createInput(int size, int bound) {
+        List l = (0..<size).collect { random.nextInt(bound)}
+        Map ordering = [:]
+        l.unique().toSorted().eachWithIndex{ int entry, int i -> ordering.put entry, i }
+        l.collect { n -> ordering[n]}
+    }
+
+    @Test
+    void testRandomized() {
+        0.upto(100) {
+            List input = createInput(5, 100)
+            def expected = slowPoisonousPlants(input)
+            def actual = PoisonousPlants.poisonousPlants(input as int[])
+            assert expected == actual: "$input expected $expected; was $actual"
+        }
     }
 
     @Test
     void testSamples() {
+        // # 0
         assert poisonousPlants('6 5 8 4 7 10 9') == 2
-        assert poisonousPlants('3 2 5 4') == 2
-        assert poisonousPlants('4 3 7 5 6 4 2') == 3
     }
 
     @Test
@@ -35,6 +88,24 @@ class TestPoisonousPlants {
         assert poisonousPlants('11 7 19 6 12 12 8 8 7 1 10 15 5 12') == 5
         // # 10
         assert poisonousPlants('2 1 3 1 4 2 1 4 3 3') == 3
+        // # 29
+        assert poisonousPlants('3 2 5 4') == 2
+        // # 30
+        assert poisonousPlants('4 3 7 5 6 4 2') == 3
+    }
+
+    @Test
+    void testTestCaseFiles() {
+
+        assert poisonousPlants(testFiles.resolve('input12.txt')) == 16
+
+        assert poisonousPlants(testFiles.resolve('input20.txt')) == 24
+
+        assert poisonousPlants(testFiles.resolve('input21.txt')) == 15
+
+        assert poisonousPlants(testFiles.resolve('input23.txt')) == 80802
+
+        assert poisonousPlants(testFiles.resolve('input28.txt')) == 3
     }
 
     @Test
@@ -42,6 +113,6 @@ class TestPoisonousPlants {
         assert poisonousPlants('1 2 2 2') == 3
         assert poisonousPlants('2 2 2 2') == 0
         assert poisonousPlants('1 1 1 2 2 2 3 3 3') == 3
-        assert poisonousPlants((0..9).reverse().toList()) == 0
+        assert poisonousPlants('5 4 3 2 1 0') == 0
     }
 }
