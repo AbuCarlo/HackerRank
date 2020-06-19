5bbb591ffd2a80b73d5f9493727edfa4ebf2f5da
Tony Nassar @ work <tony.nassar@antithesis.com>
2020-06-19 16:26:24 -0400

    Unit tests work for samples

--- HackerRankGroovy/src/test/groovy/com/aanassar/hackerrank/TestDecibinary.groovy
+++ HackerRankGroovy/src/test/groovy/com/aanassar/hackerrank/TestDecibinary.groovy
@@ -1,21 +1,80 @@
 package com.aanassar.hackerrank
 
-List toDecibinary(int x) {
-    println("toDecibinary($x)")
-    if (x == 0) 
-        return [0]
-    if (x == 1)
-        return [1]
-    final result = []
-    final int remainder = x % 2
-    for (int i = 0; i <= 8 && i < x - remainder; i += 2) {
-        def most = toDecibinary((x - i) >> 1)
+import groovy.transform.Field
+
+@Field
+static final Map<Integer, Integer> counts = [0: 1, 1: 1]
+@Field
+static final Map<Integer, List<Integer>> numbers = [0: [0], 1: [1]]
+
+int countDecibinaryNumbers(int x) {
+    Integer result = counts.get(x)
+    if (result != null)
+        return result
+    result = 0
+    for (int least = x % 2; least < 10 && least <= x; least += 2) {
+        def most = (x - least) >> 1
+        result += countDecibinaryNumbers(most)
+    }
+    counts[x] = result
+    return result
+}
+
+List getDecibinaryNumbers(int x) {
+    List result = numbers.computeIfAbsent(x, { [] })
+    if (result)
+        return result
+    // An empty list will be the one we just created.
+    for (int least = x % 2; least < 10 && least <= x; least += 2) {
+        def most = (x - least) >> 1
+        def prefixes = getDecibinaryNumbers(most)
         // Leading zeros are getting dropped.
-        most.each { result << it * 10 + i + remainder }
+        prefixes.each { result << it * 10 + least }
     }
     result.sort()
     println("toDecibinary($x) => $result")
     return result
 }
 
-toDecibinary(17)
+int nthDecibinaryNumber(int n) {
+    int count = 0
+    for (int m = 0; m < n; ++m) {
+        int c = countDecibinaryNumbers(m)
+        if (count + c < n) {
+            count += c
+        } else {
+            List numerals = getDecibinaryNumbers(m)
+            return numerals[n - count - 1]
+        }
+    }
+}
+
+assert countDecibinaryNumbers(8) == 10
+
+def eight = getDecibinaryNumbers(8)
+
+assert countDecibinaryNumbers(2) == 2
+assert countDecibinaryNumbers(3) == 2
+assert countDecibinaryNumbers(9) == 10
+
+def seventeen = getDecibinaryNumbers(17)
+assert seventeen.size() == countDecibinaryNumbers(17)
+
+def sampleInputs01 = [8,
+                      23,
+                      19,
+                      16,
+                      26,
+                      7,
+                      6]
+def sampleOutputs01 = [
+        12,
+        23,
+        102,
+        14,
+        111,
+        4,
+        11
+]
+
+println sampleInputs01.collect { nthDecibinaryNumber(it) }
